62649ea189f611226022b5d9c6a2a8608865f438
Enigma <linuzzz7@yandex.ru>
2025-10-24 14:56:31 +0300

    Задание 4

---
+++ 4-expenses/expenses.py
@@ -0,0 +1,7 @@
+expenses = [100, 123, 200, 50, 234, 766, 1500]
+all_expenses = sum(expenses)
+average_expenses = all_expenses / len(expenses)
+min_expenses = min(expenses)
+max_expenses = max(expenses)
+result = (min_expenses, max_expenses, all_expenses)
+print(result)
