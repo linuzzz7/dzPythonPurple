51cb91e4f00c8d8534e95987c2fff2b32ecd962b
Enigma <linuzzz7@yandex.ru>
2025-10-25 13:48:39 +0300

    Задание 4v2

--- 4-expenses/expenses.py
+++ 4-expenses/expenses.py
@@ -1,17 +1,19 @@
-user_input = input("Введите сумму: ").split()
+user_input = input("Введите сумму: ").lower().split()
 
 if len(user_input) == 4:
-    if user_input[0].isdigit() and user_input[2].isdigit():
+    if (user_input[0].isdigit() and user_input[1] == "руб"
+            and user_input[2].isdigit() and user_input[3] == "коп"):
         rubles = user_input[0]
-        kopecks = user_input[2]
-        print(f'{rubles}.{kopecks} P')
+        if len(user_input[2]) != 2:
+            kopecks = "0" + user_input[2]
+        print(f'{rubles}.{kopecks} {chr(8381)}')
         exit()
 
 if len(user_input) == 2:
-    if user_input[0].isdigit():
+    if user_input[0].isdigit() and user_input[1] == "руб":
         rubles = user_input[0]
         kopecks = "00"
-        print(f'{rubles}.{kopecks} P')
+        print(f'{rubles}.{kopecks} {chr(8381)}')
         exit()
-        
+
 print('Некорректный формат суммы')
