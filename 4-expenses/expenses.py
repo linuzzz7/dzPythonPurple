9f0037a4c6d830200fe3eeae251fdcd4e436380d
Enigma <linuzzz7@yandex.ru>
2025-10-25 13:59:02 +0300

    Задание 4v5

--- 4-expenses/expenses.py
+++ 4-expenses/expenses.py
@@ -1,11 +1,13 @@
 user_input = input("Введите сумму: ").lower().split()
-#
+
 if len(user_input) == 4:
     if (user_input[0].isdigit() and user_input[1] == "руб"
             and user_input[2].isdigit() and user_input[3] == "коп"):
         rubles = user_input[0]
         if len(user_input[2]) != 2:
             kopecks = "0" + user_input[2]
+        else:
+            kopecks = user_input[2]
         print(f'{rubles}.{kopecks} {chr(8381)}')
         exit()
 
@@ -16,4 +18,4 @@ if len(user_input) == 2:
         print(f'{rubles}.{kopecks} {chr(8381)}')
         exit()
 
-print('Некорректный формат суммы.')
+print('Некорректный формат суммы')
