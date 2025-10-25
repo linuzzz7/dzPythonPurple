135149a854f47b4892ea984a2c04a1adb6320289
Enigma <linuzzz7@yandex.ru>
2025-10-25 14:43:44 +0300

    Внес исправления v3

--- 4-expenses/expenses.py
+++ 4-expenses/expenses.py
@@ -1,11 +1,11 @@
 user_input = input("Введите сумму: ").lower().split()
-###
+
 if len(user_input) == 4:
     if (user_input[0].isdigit() and user_input[1] == "руб"
             and user_input[2].isdigit() and user_input[3] == "коп"):
         rubles = user_input[0]
-        if len(user_input[2]) != 2:
-            kopecks = user_input[2]
+        if len(user_input[2]) == 1:
+            kopecks = '0' + user_input[2]
         else:
             kopecks = user_input[2]
         print(f'{rubles}.{kopecks:.2} {chr(8381)}')
